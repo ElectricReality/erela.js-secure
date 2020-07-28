ee45f3a6d7515f4a5dfbdc2932cea035335ca25b
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2020-07-28 20:18:29 +0800

    Change unsecured to secured

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Manager } from "./structures/Manager";
+export { Node } from "./structures/Node";
+export { Player } from "./structures/Player";
+export { Queue } from "./structures/Queue";
+export { LoadType, Plugin, State, Structure, Utils } from "./structures/Utils";
+
